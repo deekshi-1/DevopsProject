4a5054fff0419666cd562e84999d02161205f585
deekshi-1 <deekshithc1@gmail.com>
2026-07-26 18:00:58 +0530

    bug-3

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,5 +4,3 @@ set -e
 cd docker
 docker compose build
 
-docker tag ${FRONTEND_IMAGE}:latest ${FRONTEND_IMAGE}:${BUILD_NUMBER}
-docker tag ${BACKEND_IMAGE}:latest ${BACKEND_IMAGE}:${BUILD_NUMBER}
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+docker tag ${FRONTEND_IMAGE}:latest ${FRONTEND_IMAGE}:${BUILD_NUMBER}
+docker tag ${BACKEND_IMAGE}:latest ${BACKEND_IMAGE}:${BUILD_NUMBER}
 
 docker push ${FRONTEND_IMAGE}:${BUILD_NUMBER}
 docker push ${BACKEND_IMAGE}:${BUILD_NUMBER}
