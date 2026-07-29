9bf4a842fc7ad5407cf3d018867204e4c84c6d92
deekshi-1 <deekshithc1@gmail.com>
2026-07-29 15:38:12 +0530

    ff

--- application/backend/src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-const  notFound = (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: 'Resource not found'
-    });
-};
-
-module.exports = notFound;
--- scripts/undo-rollback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl rollout undo deployment/backend -n ${NAMESPACE} || true
-kubectl rollout undo deployment/frontend -n ${NAMESPACE} || true
